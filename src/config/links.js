1b5af34a22f8c7d011e211f4a07a1288ffb78457
Alexandr Sidorenkov <Alexandr-Sidorenkov@yandex.ru>
2023-01-05 14:36:12 +0400

    add images, links

---
+++ src/config/links.js
@@ -0,0 +1,6 @@
+export const ROOT_PAGE = () => '/';
+export const CATALOG_PAGE = () => '/catalog.html';
+export const PRODUCT_PAGE = (id = 0) => `/catalog/${id}.html`;
+export const ABOUT_PAGE = () => '/about.html';
+export const CONTACTS_PAGE = () => '/contacts.html';
+export const CART_PAGE = () => '/cart.html';
