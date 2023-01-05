ecdcb990e669bc61090109f676e81a9f40f7511f
Alexandr Sidorenkov <Alexandr-Sidorenkov@yandex.ru>
2023-01-05 14:32:36 +0400

    clear react template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+// import { ROOT_PAGE as root, CATALOG_PAGE as linkToCatalog, PRODUCT_PAGE as linkToProduct, ABOUT_PAGE as linkToAbout, CONTACTS_PAGE as linkToContacts, CART_PAGE as linkToCart } from './config/links';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
