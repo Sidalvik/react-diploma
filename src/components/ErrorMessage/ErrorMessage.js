1d75c24fd87197ad48aee8c83dbbc6c018aed837
Alexandr Sidorenkov <Alexandr-Sidorenkov@yandex.ru>
2023-01-07 14:58:14 +0400

    added ErrorMessage component

---
+++ src/components/ErrorMessage/ErrorMessage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ErrorMessage(props) {
+    const {errorText} = props;
+    console.log(`Catch error: "${errorText}"`);
+
+  return (
+    <div className="alert alert-danger py-2 px-4 error-message" role="alert">
+        <p className='m-0 text-center'>
+            <span className='error-logo align-middle display-3 fs-3'>&#9888; </span>
+            <span className='error-text align-middle'>{errorText}</span>
+        </p>
+    </div>
+  )
+}
+
+ErrorMessage.defaultProps = {
+    errorText: 'Error! Возникла непредвиденная ошибка!',
+}
+
+ErrorMessage.propTypes = {
+    errorText: PropTypes.string,
+}
+
+export default ErrorMessage
